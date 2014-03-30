722d41bb724769b257031b299beff4f14db2a16d
nmonterroso <nelsonmonterroso@gmail.com>
2014-03-30 12:07:20 -0700

    added smooth switching between views

--- chrome-extension/js/controllers/route.js
+++ chrome-extension/js/controllers/route.js
@@ -1,16 +1,27 @@
-define(['angular', 'lodash'], function(ng, _) {
+define(['angular', 'lodash', 'jquery', 'angular-route'], function(ng, _, $) {
 	ng.module('iptbot')
-		.controller('RouteController', function() {
+		.controller('RouteController', ['$scope', function($scope) {
 
-		})
+		}])
+		.directive('navItem', ['$location', '$route', function($location, $route) {
+			return {
+				link: function(scope, e, attrs) {
+					$(e).click(function() {
+						window.location = window.location.origin + window.location.pathname + '#/'+attrs.location;
+						$('#nav li').removeClass('selected');
+						$(this).addClass('selected');
+					});
+				}
+			}
+		}])
 		.config(['$routeProvider', function($routeProvider) {
 			$routeProvider
 				.when('/torrents', {
-					templateUrl: 'templates/torrents.html',
+					templateUrl: 'torrents.html',
 					controller: 'TorrentListController'
 				})
 				.when('/subscriptions', {
-					templateUrl: 'templates/subscriptions.html',
+					templateUrl: 'subscriptions.html',
 					controller: 'SubscriptionController'
 				})
 				.otherwise({
